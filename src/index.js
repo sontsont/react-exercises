0f721fdb609072c92dd7084e8f699400bc2bb494
Aye Mya Han <sontsont@gmail.com>
2019-06-07 23:37:32 +0630

    Functional Components with style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyApp(){
-    return(
-        <ul>
-            <li>List one</li>
-            <li>List two</li>
-            <li>List three</li>
-        </ul>
-    );
-}
- 
-ReactDOM.render(<MyApp/>, document.getElementById('root'));
+const divStyle = {
+    color: "red",
+    background: "green"
+};
+ function MyInfo(){
+     return(<div style={divStyle}>
+         <h1>Sont Sont</h1>
+         <p>This is the about me page...</p>
+         <ul>
+             <li>Ngapali Beach</li>
+             <li>Norway</li>
+             <li>America</li>
+         </ul>
+     </div>
+
+     );
+ }
+ReactDOM.render(<MyInfo/>, document.getElementById('root'));
